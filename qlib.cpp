248f37fdce4728624e40db65169dfa56783c6940
hitode221 <marina.dr98@mail.ru>
2016-06-03 19:57:55 +0300

    Create qlib.cpp

---
+++ qlib.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> q;
+
+extern "C" {
+	__declspec(dllexport) void add(int a)
+	{
+		q.push_back(a);
+	}
+
+	__declspec(dllexport) void del()
+	{
+		if (q.size()) q.erase(q.begin());
+		else throw "ERROR: queue is empty!\n";
+	}
+
+	__declspec(dllexport) int get()
+	{
+		if (q.size()) return q[0];
+		else throw "ERROR: queue is empty!\n";
+	}
+}
