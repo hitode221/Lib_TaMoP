ed5c791114402adfd477ef3a4a55900f3c3d8e3d
hitode221 <marina.dr98@mail.ru>
2016-06-03 19:58:51 +0300

    Create qwplib.cpp

---
+++ qwplib.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> q;
+
+extern "C" {
+	__declspec(dllexport) void add(int a)
+	{
+		q.push_back(a);
+	}
+
+	__declspec(dllexport) void del()
+	{
+		if (q.size()) {
+			std::sort(q.begin(), q.end());
+			q.erase(q.begin());
+		}
+		else throw "ERROR: queue is empty!\n";
+	}
+
+	__declspec(dllexport) int get()
+	{
+		if (q.size()) {
+			std::sort(q.begin(), q.end());
+			return q[0];
+		}
+		else throw "ERROR: queue is empty!\n";
+	}
+}
